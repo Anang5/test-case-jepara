d705bb205b03eb987ecebbf124c6b25f22be2677
Wahyu Kristiawan <whykris97@gmail.com>
2022-04-06 23:39:50 +0700

    init models

---
+++ be-test/app/Models/Products.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Products extends Model
+{
+    protected $DBGroup          = 'default';
+    protected $table            = 'products';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $insertID         = 0;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = true;
+    protected $protectFields    = true;
+    protected $allowedFields    = [
+        'name',
+        'description',
+        'price',
+        'category_id',
+    ];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [
+        'name' => 'required|min_length[3]|max_length[100]',
+        'description' => 'required|min_length[3]|max_length[1000]',
+        'price' => 'required|decimal',
+        'category_id' => 'required|integer',
+    ];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+}
